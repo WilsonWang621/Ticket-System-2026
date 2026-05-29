64be2112f6e6224a2e1607cfec943b07dbb8eded
wangwenxuan <200706215314@sjtu.edu.cn>
2026-05-29 13:36:15 +0800

    Add file to .gitignore

--- main.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by lenovo on 2026/5/29.
-//
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <../include/ticket_system.h>
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+
+    return 0;
+}
